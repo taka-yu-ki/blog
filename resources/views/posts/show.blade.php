0ceb24cf740c1a1ee684bcb61d225896d4b11729
taka-yu-ki <taka7_zegs@icloud.com>
2023-10-09 14:42:47 +0900

    ブログ投稿の編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,7 +17,10 @@
                 <p>{{ $post->body }}</p>
             </div>
         </div>
-        <div class="footer">
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">edit</a>
+        </div>
+        <div class="back">
             <a href="/">戻る</a>
         </div>
     </body>
